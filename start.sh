1366b42941ff5a1fc67306e462b26b71e2498a83
Alex Kalvan <a.kalvan@anchorfree.com>
2016-12-14 15:40:02 -0800

    fixing dc getting machanism

--- start.sh
+++ start.sh
@@ -4,31 +4,31 @@ dev=`ip route get 8.8.8.8 | grep dev | awk '{print $5}'`
 ip=`ip addr show $dev | grep ^[\ \t]*inet\ | sed -e "s/^[\ \t]*inet //;s/\/.*//;s/\ peer.*//" | sed -n 1p`
 
 bucket="af-consul-config"
-bucket_list=`/opt/s3cmd/s3cmd ls s3://af-consul-config/ 2>/dev/null`
+bucket_list=`/opt/s3cmd/s3cmd ls s3://$bucket/ 2>/dev/null`
 s3_avail=$?
 if [ $s3_avail -ne 0 ]; then
     echo "S3 Bucket is unavailable (Error $s3_avail)"
     exit $s3_avail
 fi
 
-dc=`cat /consul/config/dc.json | jq -r '.datacenter'`
+peer="`hostname -s`
+dc="`echo $peer | cut -f2 -d'-'`"
 dc_exists=`echo $bucket_list | grep peers-$dc | wc -l`
-#cluster_exists=`echo $bucket_list | grep peers | wc -l`
 key_generated=`echo $bucket_list | grep key.json | wc -l`
-peer=peers-$dc-`hostname -s`
 
-    /opt/s3cmd/s3cmd get s3://af-consul-config/peers*.json /consul/config/
+/opt/s3cmd/s3cmd get s3://$bucket/peers*.json /consul/config/
 
 # Consul first run
 if [ -f /consul/firstrun ]; then
+    echo "{ \"datacenter\": \"$dc\" }"  > /consul/config/dc.json
     echo "{ \"start_join\": [\"$ip\"] }" > /consul/config/$peer.json
-    /opt/s3cmd/s3cmd get s3://af-consul-config/main.json /consul/config/main.json
+    /opt/s3cmd/s3cmd get s3://$bucket/main.json /consul/config/main.json
     /opt/s3cmd/s3cmd put /consul/config/$peer.json s3://$bucket/$peer.json
     if [ "$key_generated" -eq "0" ]; then
 	echo "{ \"encrypt\": \"`consul keygen`\" }" > /consul/config/key.json
 	/opt/s3cmd/s3cmd put /consul/config/key.json s3://$bucket/key.json
     else
-	/opt/s3cmd/s3cmd get s3://af-consul-config/key.json /consul/config/key.json
+	/opt/s3cmd/s3cmd get s3://$bucket/key.json /consul/config/key.json
     fi
     if [ "$dc_exists" -eq "0" ]; then
 	echo "{ \"bootstrap\": true }" > /consul/config/bootstrap.json
